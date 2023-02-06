e6ee675fb740ff690fc6cf9a3062445b0cf4294a
Yarden Farag <yarden.farag@gmail.com>
2023-02-07 00:22:34 +0200

    added nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = []
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error => console.error(error),
+      complete: () => {
+        console.log('request completed')
+        console.log('extra statement')
+      }
+    })
+  }
 }
