c1bbca30f4c40bd0747b78438c632aa2ff034e17
Yarden Farag <yarden.farag@gmail.com>
2023-02-06 14:32:04 +0200

    added pagination and filtering

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,8 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 app.UseStaticFiles();
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -33,9 +33,14 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationsEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);           
         }
+
     }
 }
